d3fc01aad9611f94279fb3ec1770556f53c215b3
Maksymilian Pawlak <maksymilianpawlak@Maksymilians-MacBook-Pro.local>
2018-07-16 15:50:50 +0200

    Initial Spring Boot + Citrus

---
+++ src/main/java/io/m4ks/payment/PaymentApiApplication.java
@@ -0,0 +1,12 @@
+package io.m4ks.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(io.m4ks.payment.PaymentApiApplication.class, args);
+	}
+}
